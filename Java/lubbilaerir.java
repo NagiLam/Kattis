b3f3b08d25e45afe7d38c7c27f116a8502cc8969
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2024-06-30 23:23:42 +0200

    Create lubbilaerir.java

---
+++ Java/lubbilaerir.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class lubbilaerir {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String word = in.nextLine();
+        in.close();
+
+        System.out.println(word.charAt(0));
+    }
+}
