a587aaaa6b27843651a996c7e52bd78ea7cae823
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2024-06-30 23:31:49 +0200

    Create ovissa.java

---
+++ Java/ovissa.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class ovissa {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String sound = in.nextLine();
+        in.close();
+
+        System.out.println(sound.length());
+    }
+}
