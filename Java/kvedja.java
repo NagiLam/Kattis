7655cc3f2af3c14b2da77fd71f7c7a2330548c98
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2024-06-30 23:26:53 +0200

    Create kvedja.java

---
+++ Java/kvedja.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class kvedja {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String nameSender = in.nextLine();
+        in.close();
+
+        System.out.println("Kvedja,");
+        System.out.println(nameSender);
+    }
+}
