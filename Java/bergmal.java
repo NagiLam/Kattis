0c696d84a8f9f8f2fe0b9eedde839289b7431c60
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2024-06-30 23:29:14 +0200

    Create bergmal.java

---
+++ Java/bergmal.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class bergmal {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String info = in.nextLine();
+        in.close();
+
+        System.out.println(info);
+    }
+}
