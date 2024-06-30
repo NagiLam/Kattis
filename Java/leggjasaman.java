12d9a1f986b4785d011df486fd3cdc49bd62ff44
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2024-06-30 23:12:54 +0200

    Create leggjasaman.java

---
+++ Java/leggjasaman.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class leggjasaman {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int carsArnar = in.nextInt();
+        int carsHannes = in.nextInt();
+
+        System.out.println(carsArnar + carsHannes);
+    }
+}
