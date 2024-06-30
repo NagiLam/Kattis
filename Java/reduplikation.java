610d81a3ec4dc6013cb8544f00a101977a959881
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2024-06-30 23:45:09 +0200

    Create reduplikation.java

---
+++ Java/reduplikation.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class reduplikation {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String word = in.nextLine();
+        int repeatNum = in.nextInt();
+        in.close();
+
+        for (int i = 0; i < repeatNum; i++) {
+            System.out.print(word);
+        }
+    }
+}
