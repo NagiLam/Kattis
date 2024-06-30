23886f165b2f3fdb6b85e882d5852031f1e2475b
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2024-06-30 23:20:50 +0200

    Create dragafra.java

---
+++ Java/dragafra.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class dragafra {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        in.close();
+
+        System.out.println(n - m);
+    }
+}
