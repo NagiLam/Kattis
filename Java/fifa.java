864125921c89185c3167a14c540c269481addaba
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2024-06-30 23:36:33 +0200

    Create fifa.java

---
+++ Java/fifa.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class fifa {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        in.close();
+
+        System.out.println(2022 + (n/k));
+    }
+}
