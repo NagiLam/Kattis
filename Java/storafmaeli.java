f07b53f8b106f40c7362d93a040d6292713f073e
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2024-07-01 23:58:01 +0200

    Create storafmaeli.java

---
+++ Java/storafmaeli.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class storafmaeli {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println((in.nextInt() % 10) == 0 ? "Jebb" : "Neibb");
+    }
+}
