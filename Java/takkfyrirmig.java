c5baf754e62e027824a6b1b298bf176e10d3c19f
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2024-07-02 00:17:39 +0200

    Create takkfyrirmig.java

---
+++ Java/takkfyrirmig.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class takkfyrirmig {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int numGuess = Integer.parseInt(in.nextLine());
+
+        // First approach: Use only one loop to read and print the name
+        for (int i = 0; i < numGuess; i++) {
+            String s = in.nextLine();
+            System.out.println("Takk " + s);
+        }
+      
+        // Second approach: First, create a String Array then using 1 loop
+        // to put the name into the array and 1 loop to print it
+        /*
+         * String[] nameArr = new String[numGuess];
+         * for (int i = 0; i < numGuess; i++) {
+         * nameArr[i] = in.nextLine();
+         * }
+         * 
+         * for (String name : nameArr) {
+         * System.out.println("Takk " + name);
+         * }
+         */
+    }
+}
