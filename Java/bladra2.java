a2dbf37a9d4d2c6f686704c23f0667fcd301fc86
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2024-07-01 23:51:35 +0200

    Create bladra2.java

---
+++ Java/bladra2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class bladra2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int velo = in.nextInt();
+        int accele = in.nextInt();
+        int time = in.nextInt();
+        in.close();
+
+        double result = (velo * time) + (0.5 * accele * time * time);
+        System.out.println(result);
+    }
+}
