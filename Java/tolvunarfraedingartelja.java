e47aec41bf22209a33c6d468a659670aafe0383c
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2024-07-02 22:48:08 +0200

    Create tolvunarfraedingartelja.java

---
+++ Java/tolvunarfraedingartelja.java
@@ -0,0 +1,11 @@
+// Katis - Tölvunarfræðingar telja
+import java.util.*;
+
+class tolvunarfraedingartelja {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        System.out.println(n - 1);
+    }
+}
