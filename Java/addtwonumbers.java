52a8f57b8a457ce9aca3206e17fb2e220181785c
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2024-07-03 15:48:20 +0200

    Create addtwonumbers.java

---
+++ Java/addtwonumbers.java
@@ -0,0 +1,11 @@
+// Kattis - Add Two Numbers
+import java.util.*;
+
+class addtwonumbers {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println(in.nextInt() + in.nextInt());
+        in.close();
+    }
+}
