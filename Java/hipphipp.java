4ad825f3180a80e7b9b1a2fcb256545cc8a20d97
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2024-07-04 00:41:48 +0200

    Create hipphipp.java

---
+++ Java/hipphipp.java
@@ -0,0 +1,10 @@
+// Kattis - Hipp Hipp
+import java.util.*;
+
+class hipphipp {
+    public static void main(String[] args){
+        for (int i = 0; i < 20; i++) {
+            System.out.println("Hipp hipp hurra!");   
+        }
+    }
+}
