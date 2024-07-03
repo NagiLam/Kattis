483071d559419dd5d278ace33869892394ee0124
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2024-07-03 22:57:20 +0200

    Create metronome.java

---
+++ Java/metronome.java
@@ -0,0 +1,12 @@
+// Kattis - Metronome
+import java.util.*;
+
+class metronome {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        System.out.println((double) n / 4);
+        in.close();
+    }
+}
