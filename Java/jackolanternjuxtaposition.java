b0b5d0ef03ffa3d06784f22c76274829dfbd0fcb
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2024-07-03 15:44:18 +0200

    Create jackolanternjuxtaposition.java

---
+++ Java/jackolanternjuxtaposition.java
@@ -0,0 +1,11 @@
+// Katis - Jack-O'-Lantern Juxtaposition
+import java.util.*;
+
+class jackolanternjuxtaposition {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println(in.nextInt() * in.nextInt() * in.nextInt());
+        in.close();
+    }
+}
