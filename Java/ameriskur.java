aa5773e6bed9cbcc9e7b514396b19c2066980cb9
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2024-07-04 00:08:13 +0200

    Create ameriskur.java

---
+++ Java/ameriskur.java
@@ -0,0 +1,11 @@
+// Kattis - Amerískur vinnustaður
+import java.util.*;
+
+class ameriskur {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println(in.nextInt() * 0.09144);
+        in.close();
+    }
+}
