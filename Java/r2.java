ac204a292139b4a11fbd66f7fb91390346c2b531
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2024-07-05 00:45:26 +0200

    Create r2.java

---
+++ Java/r2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class r2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int r1 = in.nextInt();
+        int r2 = in.nextInt();
+
+        System.out.println((r2 * 2) - r1);
+        in.close();
+    }
+}
