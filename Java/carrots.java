15d3a2a50823ab8c55c88ca81bb47ab711ef1624
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2024-07-05 00:04:00 +0200

    Create carrots.java

---
+++ Java/carrots.java
@@ -0,0 +1,18 @@
+// Kattis - Solving for Carrots
+import java.util.*;
+
+class carrots {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int nContestants = in.nextInt();
+        int nProblemSolved = in.nextInt();
+        in.next();
+
+        for (int i = 0; i < nContestants; i++) {
+            String s = in.nextLine();
+        }
+
+        System.out.println(nProblemSolved);
+        in.close();
+    }
+}
