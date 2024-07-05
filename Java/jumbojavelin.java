5e4befb14fd996ae4204744acfe07133dbd09689
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2024-07-05 23:11:32 +0200

    Create jumbojavelin.java

---
+++ Java/jumbojavelin.java
@@ -0,0 +1,17 @@
+ // Kattis - Jumbo Javelin
+import java.util.*;
+
+class jumbojavelin {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int nRods = Integer.parseInt(in.nextLine());
+        int sum = 0;
+
+        for (int i = 0; i < nRods; i++) {
+            sum += in.nextInt();
+        }
+
+        System.out.println(sum - (nRods - 1));
+        in.close();
+    }
+}
