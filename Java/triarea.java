71798066de767d12c6e249abb29ccecc343edc28
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2024-07-05 17:11:22 +0200

    Create triarea.java

---
+++ Java/triarea.java
@@ -0,0 +1,11 @@
+// Kattis - Triangle Area
+import java.util.*;
+
+class triarea {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println((double) 0.5 * in.nextInt() * in.nextInt());
+        in.close();
+    }
+}
