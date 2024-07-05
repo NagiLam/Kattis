cd67bf5de06a3d96cd8e3ad15d369dc3c04e3549
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2024-07-05 17:08:33 +0200

    Create twosum.java

---
+++ Java/twosum.java
@@ -0,0 +1,11 @@
+// Kattis - Two-sum
+import java.util.*;
+
+class twosum {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println(in.nextInt() + in.nextInt());
+        in.close();
+    }
+}
