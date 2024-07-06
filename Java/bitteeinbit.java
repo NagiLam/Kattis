089469be83430e31bb59cce311f0fabacf9217e5
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2024-07-06 23:04:31 +0200

    Create bitteeinbit.java

---
+++ Java/bitteeinbit.java
@@ -0,0 +1,12 @@
+// Kattis - Bitte ein Bit
+import java.util.*;
+
+class bitteeinbit {
+    public static void main(String[] argh) {
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+
+        System.out.println(input.charAt(0));
+        in.close();
+    }
+}
