c2e7f849ffd87bcf1e122b878209055e85a869a9
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2024-07-07 22:18:31 +0200

    Create modulo.java

---
+++ Java/modulo.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class modulo {
+    public static void main(String[] argh) {
+        Scanner in = new Scanner(System.in);
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < 10; i++) {
+            set.add(in.nextInt() % 42);
+        }
+
+        System.out.println(set.size());
+        in.close();
+    }
+}
