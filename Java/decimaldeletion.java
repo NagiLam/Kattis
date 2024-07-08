65f1e62407f0784b89be6543d655e17cba088336
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2024-07-08 15:10:18 +0200

    Create decimaldeletion.java

---
+++ Java/decimaldeletion.java
@@ -0,0 +1,11 @@
+// Kattis - Decimal deletion
+import java.util.*;
+
+class decimaldeletion {
+    public static void main(String[] argh) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println(Math.round(in.nextDouble()));
+        in.close();
+    }
+}
