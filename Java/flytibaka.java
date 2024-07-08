ee4467a5f137d6886e1cb5e63ba32289499e1b42
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2024-07-08 15:21:45 +0200

    Create flytibaka.java

---
+++ Java/flytibaka.java
@@ -0,0 +1,11 @@
+// Kattis - Flýtibaka
+import java.util.*;
+
+class flytibaka {
+    public static void main(String[] argh) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("");
+        in.close();
+    }
+}
