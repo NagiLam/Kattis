7aaccb4b4aea9e11bb8d04709f138c842329b14f
Lam Phuoc Huy <80982762+NagiLam@users.noreply.github.com>
2024-10-03 20:35:27 +0200

    Create thelastproblem.java

---
+++ Java/thelastproblem.java
@@ -0,0 +1,12 @@
+// Kattis - The Last Problem
+import java.util.*;
+
+public class thelastproblem {
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        String name = in.nextLine();
+        
+        System.out.println("Thank you, " + name +", and farewell!");
+        in.close();
+    }
+}
